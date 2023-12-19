108aa685617a5e3c4a4bf39395a3ab6ce6331d71
piotrvidal <piotr.vidal@gmail.com>
2023-12-18 20:25:36 -0500

    initial cns and pns commit

---
+++ nervous_system/nervous_system.rs
@@ -0,0 +1,16 @@
+
+struct NervousSystem {
+	// central_nervous_system: x,
+	// peripheral_nervous_system: y,
+}
+
+struct CentralNervousSystem {
+	// brain: x,
+	// spinal_cord: y,
+	// sensory_organs: z,	
+}
+
+struct PeripheralNervousSystem {
+	// efferent_division: x,
+	// afferent_division: y,
+}
